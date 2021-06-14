45c9689a37ccb2dc0ee1b3345630788ab7e7c41c
Eddie <eduardo.oliveira@estudante.ufjf.br>
2021-06-14 15:47:01 -0300

    mudança basica

--- src/test/java/funcionario/FuncionarioMensalistaTest.java
+++ src/test/java/funcionario/FuncionarioMensalistaTest.java
@@ -19,4 +19,21 @@ class FuncionarioMensalistaTest {
         funcionarioMensalista.setValorMes(140000.0f);
         assertEquals(140000.0f, funcionarioMensalista.calcularSalario());
     }
+
+    @Test
+    void deveCalcularSalarioDiarista() {
+        FuncionarioDiarista funcionarioDiarista = new FuncionarioDiarista();
+        funcionarioDiarista.setValorDia(300.0f);
+        funcionarioDiarista.setNumDias(25);
+        assertEquals(7500.0f, funcionarioDiarista.calcularSalario());
+    }
+
+    @Test
+    void deveCalcularSalarioHorista() {
+        FuncionarioHorista funcionarioHorista = new FuncionarioHorista();
+        funcionarioHorista.setValorHora(50.0f);
+        funcionarioHorista.setNumDias(25);
+        funcionarioHorista.setNumHorasDia(8);
+        assertEquals(10000.0f, funcionarioHorista.calcularSalario());
+    }
 }
